54f4ffe280c6859b284609b89c1af60bdc08f12a
lcaceresdbtarget <leonid.caceres@dbtarget.pe>
2024-11-12 15:20:39 -0500

    Change

--- DemoSonar/run_sonnar.bat
+++ DemoSonar/run_sonnar.bat
@@ -1,3 +1,3 @@
-dotnet sonarscanner begin /o:"usuariotmp001" /k:"usuariotmp001_demoSonar" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.token="ca62dcb0d053760142e0b51651313e66729f8716" /d:sonar.scanner.scanAll=false
+dotnet sonarscanner begin /o:"lcaceres" /k:"lcaceres_demoSonar" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.token="be54aca2db91e1478d1bf7ffeb306843cea6a4ee" /d:sonar.scanner.scanAll=false
 dotnet build
-dotnet sonarscanner end /d:sonar.token="ca62dcb0d053760142e0b51651313e66729f8716"
+dotnet sonarscanner end /d:sonar.token="be54aca2db91e1478d1bf7ffeb306843cea6a4ee"
